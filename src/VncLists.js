2dc75ec35d621a2011d5708adc3fddbee91961c5
dila ayunda <157418013+dilaayunda110210@users.noreply.github.com>
2024-02-02 16:37:26 +0000

    tambah vnc link

--- src/VncLists.js
+++ src/VncLists.js
@@ -1,6 +1,10 @@
 export const vncList = [
   { vncLink: "https://c52dvp-3000.csb.app" },
   { vncLink: "https://tnwtnf-3000.csb.app" },
+  { vncLink: "https://6yvylw-3000.csb.app" },
+  { vncLink: "https://wggf9z-3000.csb.app" },
+  { vncLink: "https://g6c2pt-3000.csb.app" },
+  { vncLink: "https://78x6zt-3000.csb.app" },
 
   // ... (tambahkan item lainnya sesuai kebutuhan)
 ];
